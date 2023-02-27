7e7c52863f858adc52f51f3a0ff07500c06dfd21
vietnguyen97 <112857700+aiflocksvn@users.noreply.github.com>
2023-02-27 12:27:16 +0700

    update domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const dotenv = require("dotenv");
+dotenv.config({
+  path: `environments/.env.${process.env.BUILD_ENV} || local}`,
+});
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
